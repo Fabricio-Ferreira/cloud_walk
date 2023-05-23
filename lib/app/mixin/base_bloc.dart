a82b9a132ef9f3a88e04d9ecfaa3c6a68860d0ea
Fabrício Ferreira da Silva <fabricio.thyala@gmail.com>
2023-05-23 00:22:12 -0300

    Fix screen block no internet

--- lib/app/mixin/base_bloc.dart
+++ lib/app/mixin/base_bloc.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:cloud_walk_test/app/styles/app_text_style.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -59,20 +60,38 @@ abstract class AppGetXController<T> extends GetxController {
 
   void showNetworkView() {
     Get.bottomSheet(
-        Container(
-          height: Get.height,
-          width: Get.width,
-          child: Container(
-            color: Colors.red,
+      Container(
+        height: Get.height,
+        width: Get.width,
+        child: Container(
+          color: Colors.blue.shade900,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Icon(Icons.info_outline, color: Colors.white, size: 50),
+                const SizedBox(height: 20),
+                Text(
+                  'Estamos sem internet, verifique se o modo Avião está ligado! Caso estiver, desligue e retorne a utilizar o nosso aplicativo...',
+                  style: AppTextStyle.headlineSmall(color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
           ),
         ),
-        isScrollControlled: true,
-        clipBehavior: Clip.antiAlias,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(20),
-            topRight: Radius.circular(20),
-          ),
-        ));
+      ),
+      isDismissible: false,
+      enableDrag: false,
+      isScrollControlled: true,
+      clipBehavior: Clip.antiAlias,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+      ),
+    );
   }
 }
